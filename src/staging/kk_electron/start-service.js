017952661e3413eeb85406ae862d3bbacf0580d1
Kostyantyn Kovalskyy <konstantin@icloak.org>
2015-11-18 17:34:17 -0500

    Added my electron talker to staging.

---
+++ src/staging/kk_electron/start-service.js
@@ -0,0 +1,30 @@
+var child_process = require('child_process')
+var child = 0;
+
+function log(data, lvl) {
+   console.log(data);
+}
+
+function send(data) {
+   if (child == 0)
+      log('error: python child is null');
+   child.stdin.write(JSON.stringify(data) + '\n');
+}
+
+function init(on_data) {
+   child = child_process.spawn(__dirname + '/../../../gui_server.py')
+   //child.stdin.write(data);
+   child.stderr.on('data', function(data) {
+      log('python ouput:' + data);
+   });
+   child.stdout.on('data', function(data) {
+      //document.write(data);
+      var msg = JSON.parse(data);
+      if (msg['magic'] != 'gentoo_sicp_rms')
+         log('error: bad python message');
+      on_data(msg);
+   });
+}
+
+exports.init = init
+exports.send = send
