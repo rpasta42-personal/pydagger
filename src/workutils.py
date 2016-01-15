9d683f8bf9fee3f7f5746c1ca3c84042bb2e2f8e
Felipe Orellana <forellana@icloak.org>
2016-01-15 14:19:55 -0500

    Added label and ignore_error arguments to DeferredExec.add method

---
+++ src/workutils.py
@@ -0,0 +1,27 @@
+from pycloak.events import Event
+
+import logging
+logger = logging.getLogger(__name__)
+
+class DeferredProgress(object):
+
+   def __init__(self):
+      self.on_progress = Event()
+      self.fn = list()
+      super(DefferedProgress, self).__init__()
+
+   def add(self, fn, args=(), kwargs=dict(), label=None, ignore_errors=False):
+      self.fn.append((fn, args, kwargs, label, ignore_errors)) # storing function argument and kw arguments as tuple here
+
+   def exec(self):
+      total_work = len(self.fn)
+      count = 0
+      for fn, args, kwargs, label, ignore_errors in self.fn:
+         try:
+            fn(*args, **kwargs)
+         except Exception as e:
+            if not ignore_errors:
+               raise e
+
+         self.on_progress((count * 100 / total_work), total_work, count, label)
+         count += 1
